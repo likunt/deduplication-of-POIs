d324d1cd7376db69648fda425f5f098814d18690
likunt <tlk0812@hotmail.com>
2024-03-08 11:00:02 -0500

    Create config.py

---
+++ sagemaker/custom_pipeline/config.py
@@ -0,0 +1,29 @@
+POI_TYPES = {"dining", "experience", "lodging"}
+STOPWORDS_COMMON = ["the", "and", "by"]
+STOPWORDS_LODGING = ["hotel", "hostel", "hostal", "inn", "motel", "resort", "lodge", "villa"]
+STOPWORDS_DINING = ["restaurant", "bar", "tavern", "cafe", "bistro", "diner", "pub", "eatery", "cafeteria", "steakhouse"]
+STOPWORDS_EXPERIENCE = ["park", "aquarium", "gallery", "center", "site", "theater", "land"]
+THRESHOLD = 0.7
+DIST_TOL = 0.01
+FILL_VALUE = 0.99 
+ADDRESS_ABBREVIATIONS = {"rd": "road",
+                        "st": "street",
+                        "dr": "drive",
+                        "ave": "avenue",
+                        "blvd": "boulevard",
+                        "ln": "lane",
+                        "ct": "court",
+                        "pl": "place",
+                        "ter": "terrace",
+                        "pkwy": "parkway",
+                        "cir": "circle",
+                        "jr": "junior",
+                        "ne": "northeast",
+                        "nw": "northwest",
+                        "se": "southeast",
+                        "sw": "southwest",
+                        "e": "east",
+                        "w": "west",
+                        "s": "south",
+                        "n": "north"}
+
